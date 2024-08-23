1bb89e03582e458569b96c6c47b3936444546384
Olga rybak <lelenka2022@mail.ru>
2024-08-23 14:56:10 +0300

    added files

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+    public class Main {
+        public static void main(String[] args) 
+	{
+            int priceticket = 13676; // стоимость билета
+            int miles = 20; // количество рублей для одной бонусной мили
+    System.out.println("Бонусных миль начислено:");
+    System.out.println((priceticket / miles) + " бонусных мили (-я)"); // Рассчитываем количество бонусных миль
+
+        }
+    }
