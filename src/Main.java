344d5ea0584210236470b8b6766d671d713ee4a5
Olga rybak <lelenka2022@mail.ru>
2024-08-23 23:00:58 +0300

    modified files

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,9 @@
-    public class Main {
-        public static void main(String[] args) 
-	{
-            int priceticket = 13676; // стоимость билета
-            int miles = 20; // количество рублей для одной бонусной мили
-    System.out.println("Бонусных миль начислено:");
-    System.out.println((priceticket / miles) + " бонусных мили (-я)"); // Рассчитываем количество бонусных миль
+public class Main {
+    public static void main(String[] args) {
+        int priceticket = 13676;
+        int miles = 20;
+        System.out.println("Бонусных миль начислено:");
+        System.out.println((priceticket / miles) + " бонусных мили (-я)");
 
-        }
     }
+}
